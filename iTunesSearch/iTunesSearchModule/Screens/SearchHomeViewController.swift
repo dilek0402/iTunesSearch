15410b420be53c7852191cae99f347da0955bef1
Dilek Eminoğlu <dilek0402@hotmail.com>
2021-12-13 18:38:37 +0300

    Added SearchBar and CollectionView in Search Home View Controller

--- iTunesSearch/iTunesSearchModule/Screens/SearchHomeViewController.swift
+++ iTunesSearch/iTunesSearchModule/Screens/SearchHomeViewController.swift
@@ -12,7 +12,11 @@ final class SearchHomeViewController: UIViewController {
     // MARK: - Constant
     
     private enum Constant {
-        
+        static let reuseModuleItemIdentifier : String = "moduleCell"
+        static let searchBarSize: CGFloat = 50
+        static let title = "iTunes Search"
+        static let delayTime = 800
+        static let margin: CGFloat = 10
     }
     
     // MARK: - Properties
@@ -21,17 +25,28 @@ final class SearchHomeViewController: UIViewController {
     
     // MARK: - Layout Properties
     
-  /*  private var searchBar: UISearchBar = {
+    private var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.translatesAutoresizingMaskIntoConstraints = false
         return searchBar
     }()
     
-    private var searchCollectionView: UICollectionView = {
-        let collectionView = UICollectionView()
+    var collectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = 0.0;
+        layout.minimumLineSpacing = 0.0;
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.clear
         collectionView.translatesAutoresizingMaskIntoConstraints = false
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.minimumLineSpacing = 15
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.register(iTunesInfoCollectionViewCell.self,
+                                forCellWithReuseIdentifier: Constant.reuseModuleItemIdentifier)
         return collectionView
-    }()*/
+    }()
     
     
     
@@ -39,19 +54,95 @@ final class SearchHomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        viewModel.fetchMedia()
+        applyStyling()
+        setupUI()
+        setupConstraints()
+        setupCollectionView()
+        viewModel.delegate = self
+        
     }
     
     // MARK: - Private  Methods
     
     private func setupUI() {
+        self.view.addSubview(searchBar)
+        self.view.addSubview(collectionView)
+    }
+    
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        searchBar.delegate = self
+    }
+    
+    private func applyStyling() {
+        self.view.backgroundColor = Theme.Palette.backgroundColor
+        self.title = Constant.title
+    }
+    
+    private func setupConstraints() {
+        self.view.addConstraints([searchBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+                                  searchBar.bottomAnchor.constraint(equalTo: self.collectionView.topAnchor),
+                                  searchBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+                                  searchBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+                                  searchBar.heightAnchor.constraint(equalToConstant: Constant.searchBarSize)])
+        
+        self.view.addConstraints([collectionView.topAnchor.constraint(equalTo: self.searchBar.bottomAnchor),
+                                  collectionView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+                                  collectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+                                  collectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)])
+        
         
     }
     
-    private func makeAutoLayout() {
+    private func search(searchValue: String) {
+        viewModel.fetchMedia(searchValue: searchValue)
+    }
+}
+
+extension SearchHomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constant.reuseModuleItemIdentifier,
+                                                         for: indexPath) as? iTunesInfoCollectionViewCell {
+            cell.configureCell(item: viewModel.images[indexPath.row])
+            return cell
+        }
+        return UICollectionViewCell()
         
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        viewModel.images.count
+    }
+}
+
+extension SearchHomeViewController: SearchHomeViewModelDelegate {
+    func imageLoaded() {
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+}
+
+extension SearchHomeViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar,
+                   textDidChange searchText: String) {
+        guard  !searchText.isEmpty else {
+            return
+        }
+        if searchText.count > 2 {
+            let debouncedFunc = searchBar.debounce(interval: Constant.delayTime,
+                                                   queue: .main) {
+                self.search(searchValue: searchText)
+            }
+            debouncedFunc()
+        }
+    }
 }
 
